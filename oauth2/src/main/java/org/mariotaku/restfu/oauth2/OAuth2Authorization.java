316b940dcd07dbce4dcdc2caef91e6b1834dcf59
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-04-18 19:24:35 +0800

    supports oauth2

---
+++ oauth2/src/main/java/org/mariotaku/restfu/oauth2/OAuth2Authorization.java
@@ -0,0 +1,32 @@
+package org.mariotaku.restfu.oauth2;
+
+import org.jetbrains.annotations.NotNull;
+import org.mariotaku.restfu.RestRequest;
+import org.mariotaku.restfu.http.Authorization;
+import org.mariotaku.restfu.http.Endpoint;
+
+/**
+ * Created by mariotaku on 2017/4/18.
+ */
+public class OAuth2Authorization implements Authorization {
+
+    private final String accessToken;
+
+    public OAuth2Authorization(@NotNull String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    public String getHeader(Endpoint endpoint, RestRequest info) {
+        return "Bearer " + accessToken;
+    }
+
+    @Override
+    public boolean hasAuthorization() {
+        return true;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
