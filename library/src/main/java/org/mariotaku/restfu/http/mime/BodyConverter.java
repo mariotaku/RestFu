0b6a8182ccdb0f6fa5a03525da321cc709a12245
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-05-27 15:46:21 +0800

    made BodyConverter returns nullable

--- library/src/main/java/org/mariotaku/restfu/http/mime/BodyConverter.java
+++ library/src/main/java/org/mariotaku/restfu/http/mime/BodyConverter.java
@@ -1,12 +1,13 @@
 package org.mariotaku.restfu.http.mime;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mariotaku.restfu.http.MultiValueMap;
 
 /**
  * Created by mariotaku on 2017/4/19.
  */
 public interface BodyConverter {
-    @NotNull
+    @Nullable
     Body convert(@NotNull MultiValueMap<Body> params, @NotNull String[] args);
 }
